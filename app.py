d51a736415b696210d7a81bef8f783ad33bf4325
Ushakov Egor <usakovegor2002@gmail.com>
2025-05-19 23:17:36 +0300

    added vectorstore using HuggingFaceInstructEmbeddings

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -20,6 +22,11 @@ def get_text_chunks(text):
     )
     chunks = text_splitter.split_text(text)
     return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embeddings=embeddings)
+    return vectorstore
 
 def main():
     load_dotenv()
@@ -41,6 +48,7 @@ def main():
                 text_chunks = get_text_chunks(raw_text)
                 
                 # create vector store
+                vectorstore = get_vectorstore(text_chunks)
 
 
 if __name__ == "__main__":
