7e8261e9484c648490be06cc85f4a6b45733b9e3
Ushakov Egor <usakovegor2002@gmail.com>
2025-05-19 22:31:59 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title="Chat with your PDF", page_icon=":books:")
+
+    st.header("Chat with your PDF :books:")
+    st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload ypur PDFs here and click on 'Process'", accept_multiple_files=True)
+        st.button("Process")
+
+
+if __name__ == "__main__":
+    main()
