44f516ec0729d3a1a7a325e6e73ae4ed520db7dd
Ushakov Egor <usakovegor2002@gmail.com>
2025-05-20 23:16:26 +0300

    added conversation persistent

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import OpenAI
+import os
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -32,9 +37,27 @@ def get_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+def get_conversation_chain(vectorstore):
+    load_dotenv()
+    llm = OpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        base_url=os.getenv("OPENAI_API_BASE_URL"),
+    )
+
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with your PDF", page_icon=":books:")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
 
     st.header("Chat with your PDF :books:")
     st.text_input("Ask a question about your documents:")
@@ -53,6 +76,10 @@ def main():
                 
                 # create vector store
                 vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+    st.session_state.conversation
 
 
 if __name__ == "__main__":
