5b9bae86c35907607caf840832923c8153df73eb
Ushakov Egor <usakovegor2002@gmail.com>
2025-05-24 13:56:54 +0300

    modifieed chat history

--- app.py
+++ app.py
@@ -56,7 +56,13 @@ def get_conversation_chain(vectorstore):
 
 def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
-    st.write(response)
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i%2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():
@@ -67,13 +73,13 @@ def main():
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     st.header("Chat with your PDF :books:")
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
-
-    st.write(user_template.replace("{{MSG}}", "Hello, Robot!"), unsafe_allow_html=True)
-    st.write(bot_template.replace("{{MSG}}", "Hello, Human!"), unsafe_allow_html=True)
 
     with st.sidebar:
         st.subheader("Your documents")
@@ -92,8 +98,6 @@ def main():
 
                 # create conversation chain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
-    st.session_state.conversation
-
 
 if __name__ == "__main__":
     main()
