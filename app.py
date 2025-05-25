171643c6d6bdd8808266cfe3f2472551d0556cbf
Ushakov Egor <usakovegor2002@gmail.com>
2025-05-25 08:56:24 +0300

    fixed some bugs

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def get_conversation_chain(vectorstore):
 
 
 def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
+    response = st.session_state.conversation.invoke({'question': user_question})
     st.session_state.chat_history = response['chat_history']
 
     for i, message in enumerate(st.session_state.chat_history):
